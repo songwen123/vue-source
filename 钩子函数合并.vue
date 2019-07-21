8f4f125f0e72ae3a34ab904ae0adbbbdccbd245b
songwen123 <53135691+songwen123@users.noreply.github.com>
2019-07-21 19:48:40 +0800

    Create 钩子函数合并.vue

---
+++ 钩子函数合并.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+ 
+<script>
+import {Mixins} from '@/mixin.js';
+export default {
+  mixins: [Mixins],
+  data () {
+    return {
+      msg: 'aaaa'
+    }
+  },
+  created() {
+    console.log('#app Created')
+  }
+}
+</script>
