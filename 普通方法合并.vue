9c7c49ee62305a21277fb368bbf461fd9e521bff
songwen123 <53135691+songwen123@users.noreply.github.com>
2019-07-21 19:55:37 +0800

    Create 普通方法合并.vue
    当混合值为对象的选项时，例如 methods、components、directive，将被混合为同一个对象，两个对象键名冲突时，取组件对象的键值对

---
+++ 普通方法合并.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+ 
+<script>
+import {Mixins} from '@/mixin.js';
+export default {
+  mixins: [Mixins],
+  data () {
+    return {
+      msg: 'I am #app',
+      msg2: 'I am msg2'
+    }
+  },
+  created() {
+    console.log(this.msg)
+    console.log('我是混入对象中的变量：' + this.msg1)
+  },
+   methods: {
+        mixin: function () {
+            console.log('#app')
+        }
+    },
+    mounted() {
+        this.mixin()
+        this.mixinTwo()
+    }
+}
+</script>
