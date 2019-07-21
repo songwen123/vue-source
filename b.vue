76de3567bd168250d52e01217b80a8be59cc9764
songwen123 <53135691+songwen123@users.noreply.github.com>
2019-07-21 19:41:14 +0800

    Create b.vue

---
+++ b.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div @click="toggleShow()">aaaaaaaaaaaa</div>
+    <div>{{isshowing}}</div>
+    <div>{{msg1}}</div>
+    <div>{{hello}}</div>
+  </div>
+</template>
+ 
+<script>
+import {toggle,mixOne, mixTwo} from '@/mixin.js';
+export default {
+  mixins: [toggle,mixOne, mixTwo],
+  created() {
+    this.test();
+    this.test2();
+    console.log("这是b.vue 的create");
+  },
+  mounted () {
+      
+  },
+  data () {
+    return {
+      msg: 'bbb',
+      msg1: '这是b.vue的msg数据',
+      hello: '这是b.vue的hello数据'
+      
+    }
+  },
+  methods: {
+    test(){
+      console.log("这是b.vue的 test");
+    },
+  }
+}
+</script>
